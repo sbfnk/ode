facdd34b410bfa0a9ae151da0d516d6072b22ee3
erez <>
2006-11-13 14:14:42 +0000

    adjusted for call by reference to io routines, and documented.

--- erez/ode_io_utils.cc
+++ erez/ode_io_utils.cc
@@ -1,19 +1,17 @@
-/********************************************************************/  
+/******************************************************************/  
 
-#include "ode_io_utils.h"
-#include "ode.h"
-#include "van_der_pol.h"
+#include "ode_io_utils.hh"
+#include "ode.hh"
+#include "van_der_pol.hh"
 
 using namespace std;
 
-/********************************************************************/  
+/******************************************************************/  
 
-void ReadOdeParams(const char *ifile_name, Ode *obj)
+void ReadOdeParams(const char *ifile_name, Ode& obj)
 {
    string line;
    ifstream ifile;
-   
-   /*****/
    
    // open init file
    cout << "... opening ode input file: " << ifile_name;
@@ -29,8 +27,6 @@ void ReadOdeParams(const char *ifile_name, Ode *obj)
       cout << " ... done\n";
    }
    
-   /*****/
-   
    // reading Ode content of init.dat
    cout << "... reading ode parameters"; 
    
@@ -41,34 +37,35 @@ void ReadOdeParams(const char *ifile_name, Ode *obj)
       i=line.find("Ode parameters"); // looking for "Ode ... %%%"
    }
    
-   /* ode output file name */
-   obj->SetoFileName(read_str_val(&ifile));
+   // ode output file name 
+   obj.SetoFileName(read_str_val(ifile));
   
-   /* tmax */
-   obj->SetTmax(read_dbl_val(&ifile));
-  
-   /* dt */
-   obj->SetDt(read_dbl_val(&ifile));
-  
-   /* nsave */
-   obj->SetNsave(read_int_val(&ifile));
-  
-   /* nvars */
-   obj->SetNvars(read_int_val(&ifile));
-
-   /* step_algo */
-   obj->SetStepAlgo(read_str_val(&ifile));
-
-   /* abs_tol, rel_tol */
-   obj->SetTol(read_dbl_val(&ifile),read_dbl_val(&ifile));
+   // tmax 
+   obj.SetTmax(read_dbl_val(ifile));
    
-   /* ode i.c. file name */
-   obj->SeticFileName(read_str_val(&ifile));
+   // dt 
+   obj.SetDt(read_dbl_val(ifile));
+   
+   // nsave 
+   obj.SetNsave(read_int_val(ifile));
+   
+   // nvars 
+   obj.SetNvars(read_int_val(ifile));
+
+   // step_algo 
+   obj.SetStepAlgo(read_str_val(ifile));
+
+   // abs_tol 
+   obj.SetAbsTol(read_dbl_val(ifile));
+
+   // rel_tol 
+   obj.SetRelTol(read_dbl_val(ifile));
+   
+   // ode i.c. file name 
+   obj.SeticFileName(read_str_val(ifile));
 
    cout << " ... done\n";
    
-   /*****/
-  
    // closing init.dat
    cout << "... closing ode input file: " << ifile_name;
   
@@ -77,15 +74,13 @@ void ReadOdeParams(const char *ifile_name, Ode *obj)
       cout << " ... done\n";
 }
 
-/********************************************************************/  
+/******************************************************************/  
 
-void ReadModelParams(const char *ifile_name, VanDerPol *obj)
+void ReadModelParams(const char *ifile_name, VanDerPol& obj)
 {
    string line;
    ifstream ifile;
    ModelParams *model_params = new ModelParams;
-   
-   /*****/
    
    // open init file
    cout << "... opening model input file: " << ifile_name;
@@ -101,63 +96,59 @@ void ReadModelParams(const char *ifile_name, VanDerPol *obj)
       cout << " ... done\n";
    }
    
-   /*****/
-   
    // reading Model content of init.dat   
    cout << "... reading model parameters"; 
-
+   
    int i=-1;
    while (i<0)
    {
       getline(ifile,line); // reading a line
       i=line.find("Model parameters"); // looking for "Model ... %%%"
    }
+   
+   // beta_d    
+   model_params->beta_d=read_dbl_val(ifile);
   
-   /* beta_d */   
-   model_params->beta_d=read_dbl_val(&ifile);
+   // gamma_d 
+   model_params->gamma_d=read_dbl_val(ifile);
   
-   /* gamma_d */
-   model_params->gamma_d=read_dbl_val(&ifile);
-  
-   /* delta_d */
-   model_params->delta_d=read_dbl_val(&ifile);
+   // delta_d 
+   model_params->delta_d=read_dbl_val(ifile);
       
-   /* beta_i */
-   model_params->beta_i=read_dbl_val(&ifile);
+   // beta_i 
+   model_params->beta_i=read_dbl_val(ifile);
    
-   /* gamma_i */
-   model_params->gamma_i=read_dbl_val(&ifile);
+   // gamma_i 
+   model_params->gamma_i=read_dbl_val(ifile);
    
-   /* delta_i */
-   model_params->delta_i=read_dbl_val(&ifile);
+   // delta_i 
+   model_params->delta_i=read_dbl_val(ifile);
    
-   /* beta_m */
-   model_params->beta_m=read_dbl_val(&ifile);
+   // beta_m 
+   model_params->beta_m=read_dbl_val(ifile);
    
-   /* alpha */
-   model_params->alpha=read_dbl_val(&ifile);
+   // alpha 
+   model_params->alpha=read_dbl_val(ifile);
    
-   /* nu */
-   model_params->nu=read_dbl_val(&ifile);
+   // nu 
+   model_params->nu=read_dbl_val(ifile);
    
-   /* lambda */
-   model_params->lambda=read_dbl_val(&ifile);
+   // lambda 
+   model_params->lambda=read_dbl_val(ifile);
    
-   /* mu1 */
-   model_params->mu1=read_dbl_val(&ifile);
+   // mu1 
+   model_params->mu1=read_dbl_val(ifile);
    
-   /* mu2 */
-   model_params->mu2=read_dbl_val(&ifile);
+   // mu2 
+   model_params->mu2=read_dbl_val(ifile);
 
-   /* njac */
-   model_params->njac=obj->GetNvars();
+   // njac 
+   model_params->njac=obj.GetNvars();
 
    cout << " ... done\n";
    
-   /*****/
-
-   /* passing a pointer to model_params to Ode class member _params */
-   obj->SetModelParams(static_cast<void *>(model_params));
+   // passing a pointer to model_params to Ode class member _params 
+   obj.SetModelParams(static_cast<void *>(model_params));
    
    // closing init.dat
    cout << "... closing model input file: " << ifile_name;
@@ -167,13 +158,13 @@ void ReadModelParams(const char *ifile_name, VanDerPol *obj)
       cout << " ... done\n";
 }
 
-/********************************************************************/  
+/******************************************************************/  
 
-int read_int_val(ifstream *ifile)
+int read_int_val(ifstream& ifile)
 {
    string line, str_tmp;
    
-   getline(*ifile,line); 
+   getline(ifile,line); 
    int i=line.find("="); // looking for the =  
    if(i>=0) // check if = was found 
    {
@@ -188,13 +179,13 @@ int read_int_val(ifstream *ifile)
    return atoi(str_tmp.c_str());
 }
 
-/********************************************************************/  
+/******************************************************************/  
 
-double read_dbl_val(ifstream *ifile)
+double read_dbl_val(ifstream& ifile)
 {
    string line, str_tmp;
    
-   getline(*ifile,line); 
+   getline(ifile,line); 
    int i=line.find("="); // looking for the =  
    if(i>=0) // check if = was found 
    {
@@ -210,14 +201,14 @@ double read_dbl_val(ifstream *ifile)
 
 }
 
-/********************************************************************/
+/******************************************************************/
 
-const char *read_str_val(ifstream *ifile)
+const char *read_str_val(ifstream& ifile)
 {
    string line, str_tmp;
    char *cstr=new char[64];
    
-   getline(*ifile,line); 
+   getline(ifile,line); 
    int i=line.find("="); // looking for the =    
    if(i>=0) // check if = was found
    {
@@ -233,7 +224,7 @@ const char *read_str_val(ifstream *ifile)
    return cstr;
 }
 
-/********************************************************************/
+/******************************************************************/
 
 
 
