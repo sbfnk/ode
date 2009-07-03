df57cbf49ac974a78f56f16a55d7728412e952fa
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2009-07-03 07:50:13 +0000

    clean up dir structure

--- sleep_ode/reported.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cat $1 | awk 'BEGIN{prev=0;diff=0;curyear=0}{if (curyear == 0) {curyear = int($1);} else while ($1 >= (curyear + 1)) {if (prev>0) {diff=$4-prev} else {prev=$4}; print curyear" "$2" "$3" "$4" "diff;curyear = curyear + 1}}'
--- sleep_ode/scan_params.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-for (my $beta=0;$beta<1;$beta=$beta+0.01) {
-  for (my $rho=0;$rho<1;$rho=$rho+0.01) {
-    system "./solve_ode.x --file-id angola3 --ic-file angola3.ic --nsave 1 --dt=0.1 --N 2500000 -p angola3.prm -m angola3.mpm  --beta $beta --rho $rho > /dev/null 2>&1";
-    system "./reported.sh angola3.dat > reported.dat";
-    print "$beta $rho ".`./least_squares.sh reported.dat recorded.dat`;
-  }
-}
