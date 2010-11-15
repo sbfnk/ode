1569f8c631bc314b89e10f3b9a016134337a61cb
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2010-11-15 11:35:06 +0000

    rename

--- netode/compare_nb.sh
+++ netode/compare_nb.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-./bin/solve_ode.x --susfunc 0 $* > /dev/null
+./bin/net_ode --susfunc 0 $* > /dev/null
 first=$(awk -f maxtime.awk try1.dat)
-./bin/solve_ode.x --susfunc 1 $* > /dev/null
+./bin/net_ode --susfunc 1 $* > /dev/null
 second=$(awk -f maxtime.awk try1.dat)
 echo $(echo "$first - $second" | bc) $first $second
 
